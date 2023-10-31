35194fbc126042f3fee5bbff180e99f230e50bd6
Gogs <gogs@fake.local>
2023-10-31 00:06:14 -0300

    first, not working, version of rust snake

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,18 @@
 use comfy::*;
+use rand::Rng;
 
-pub const SCREEN_WIDTH: u32 = 600; 
+pub const SCREEN_WIDTH: u32 = 600;
 pub const SCREEN_HEIGHT: u32 = 600;
 
-pub const WORLD_WIDTH: u32 = 30; 
-pub const WORLD_HEIGHT: u32 = 30;
-pub const HALF_WORLD_WIDTH: f32 = WORLD_WIDTH as f32/2.0; 
-pub const HALF_WORLD_HEIGHT: f32 = WORLD_HEIGHT as f32/2.0;
+pub const WORLD_WIDTH: f32 = 30.0;
+pub const WORLD_HEIGHT: f32 = 30.0;
+pub const HALF_WORLD_WIDTH: f32 = WORLD_WIDTH / 2.0;
+pub const HALF_WORLD_HEIGHT: f32 = WORLD_HEIGHT / 2.0;
 
-pub const SCALE: f32 = 1.0;
+pub const SCALE: f32 = 2.0;
+pub const OFFSET: f32 = SCALE / 2.0;
 
-fn constrain(value:f32, min:f32, max:f32) -> f32 {
+fn constrain(value: f32, min: f32, max: f32) -> f32 {
     if value > max {
         return max;
     } else if value < min {
@@ -19,91 +21,164 @@ fn constrain(value:f32, min:f32, max:f32) -> f32 {
     return value;
 }
 
-pub struct Game {
-    pub _snake : Snake,
-    pub _fruit : Fruit,
- }
-
- impl Game {
-    pub fn create() -> Self {
-       Self {
-          _snake: Snake::create(), 
-          _fruit: Fruit::create(),
-       }
+fn apply_offset(value: f32) -> f32 {
+    if value % SCALE != 0.0 {
+        if value > 0.0 {
+            return value - OFFSET;
+        } else if value < 0.0 {
+            return value + OFFSET;
+        } else {
+            return value - OFFSET;
+        }
+    } else {
+        return value;
     }
- }
+}
 
-pub struct Snake 
-{
-     px: f32,
-     py: f32,
-     sx: f32,
-     sy: f32,
-     z: i32, 
-     size: f32,
-     color: Color,
+pub struct Game {
+    pub _snake: Snake,
+    pub _fruit: Fruit,
+}
+
+impl Game {
+    pub fn create() -> Self {
+        Self {
+            _snake: Snake::create(),
+            _fruit: Fruit::create(),
+        }
+    }
+}
+
+pub struct Snake {
+    pub px: f32,
+    pub py: f32,
+    sx: f32,
+    sy: f32,
+    z: i32,
+    dimension: f32,
+    color: Color,
+    pub size: usize,
+    pub tail: Vec<(f32, f32)>,
 }
 
 impl Snake {
-
     pub fn create() -> Self {
         Self {
-            px: 0.0,
-            py: 0.0,
+            px: SCALE,
+            py: SCALE,
             sx: 0.0,
             sy: 1.0,
-            z: 0,
-            size: 1.0,
+            z: 1,
+            dimension: SCALE,
             color: GREEN,
+            size: 0,
+            tail: Vec::new(),
         }
     }
 
-    pub fn set_speed(&mut self, _sx:f32, _sy:f32) {
+    pub fn set_speed(&mut self, _sx: f32, _sy: f32) {
         self.sx = _sx;
         self.sy = _sy;
     }
 
     pub fn update(&mut self) {
+        if self.tail.len() == self.size && self.tail.len() > 0 {
+            for i in 0..self.tail.len()-1 {
+                self.tail[i] = self.tail[i + 1];
+            }
+        }
+        if self.size != self.tail.len() {
+            self.tail[self.size-1] = (self.px, self.py);
+        }
+        /*
+        // check if new segment will be added
+        if self.size != self.tail.len() {
+            self.tail.push((self.px, self.py));
+        }
+        */
         self.px = self.px + self.sx * SCALE;
         self.py = self.py + self.sy * SCALE;
 
-       self.px = constrain(self.px, -(HALF_WORLD_WIDTH - self.size), HALF_WORLD_WIDTH - self.size);
-       self.py = constrain(self.py, -(HALF_WORLD_HEIGHT - self.size), HALF_WORLD_HEIGHT - self.size);
+        self.px = constrain(
+            self.px,
+            -HALF_WORLD_WIDTH + OFFSET,
+            HALF_WORLD_WIDTH - OFFSET,
+        );
+        self.py = constrain(
+            self.py,
+            -HALF_WORLD_HEIGHT + OFFSET,
+            HALF_WORLD_HEIGHT - OFFSET,
+        );  
+    }
+
+    pub fn check_eat(&mut self, fx: f32, fy: f32) -> bool {
+        if self.px == fx && self.py == fy {
+            self.size += 1;
+            return true;
+        }
+        return false;
     }
 
     pub fn draw(&self) {
-        draw_rect(vec2(self.px, self.py), splat(self.size), self.color, self.z);
-        draw_text(format!("px:{}, py:{}", self.px, self.py).as_str(), vec2(12.0,12.0), WHITE, TextAlign::Center);
+        for i in 0..self.tail.len() {
+            draw_rect(
+                vec2(self.tail[i].0, self.tail[i].1),
+                splat(self.dimension),
+                self.color,
+                self.z,
+            );
+        }
+
+        draw_rect(
+            vec2(self.px, self.py),
+            splat(self.dimension),
+            self.color,
+            self.z,
+        );
+     
     }
 }
 
-
 pub struct Fruit {
-    px: f32,
-    py: f32,
-    z: i32, 
-    size: f32,
+    pub px: f32,
+    pub py: f32,
+    z: i32,
+    dimension: f32,
     color: Color,
 }
 
 impl Fruit {
-
     pub fn create() -> Self {
-        let col : f32 = f32::floor(WORLD_WIDTH as f32/SCALE);
-        let row: f32 = f32::floor(WORLD_HEIGHT as f32/SCALE);
-        let x : f32 = f32::floor(random()*col);
-        let y : f32 = f32::floor(random()*row);
+        let mut rng = rand::thread_rng();
+        let mut x = f32::floor(rng.gen_range(-HALF_WORLD_WIDTH..=HALF_WORLD_WIDTH));
+        let mut y = f32::floor(rng.gen_range(-HALF_WORLD_HEIGHT..=HALF_WORLD_HEIGHT));
+        x = apply_offset(x); // correct x position if necessary according with SCALE
+        y = apply_offset(y); // correct y position if necessary according with SCALE
         Self {
             px: x,
             py: y,
             z: 0,
-            size: 1.0,
+            dimension: SCALE,
             color: RED,
         }
     }
 
+    pub fn new_pos(&mut self) {
+        let mut rng = rand::thread_rng();
+        let mut x = f32::floor(rng.gen_range(-HALF_WORLD_WIDTH..=HALF_WORLD_WIDTH));
+        let mut y = f32::floor(rng.gen_range(-HALF_WORLD_HEIGHT..=HALF_WORLD_HEIGHT));
+        x = apply_offset(x); // correct x position if necessary according with SCALE
+        y = apply_offset(y); // correct y position if necessary according with SCALE
+        self.px = x as f32;
+        self.py = y as f32;
+    }
+
     pub fn draw(&self) {
-        draw_rect(vec2(self.px, self.py), splat(self.size), self.color, self.z);
-        draw_text(format!("px:{}, py:{}", self.px, self.py).as_str(), vec2(12.0,12.0), WHITE, TextAlign::Center);
+        draw_rect(
+            vec2(self.px, self.py),
+            splat(self.dimension),
+            self.color,
+            self.z,
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use comfy::*;
 mod game;
 use game::*;
 
-
 simple_game!("Snake", GameState, config, setup, update);
 
+pub const TICK: u64 = 10;
 
 struct SmallGlobalState {
-   pub game: Game,
+    pub game: Game,
+    pub tick: u64,
 }
 
 pub struct GameState {}
@@ -19,39 +20,70 @@ impl GameState {
 }
 
 static STATE: Lazy<AtomicRefCell<SmallGlobalState>> = Lazy::new(|| {
-   AtomicRefCell::new(SmallGlobalState { game : Game::create() })
+    AtomicRefCell::new(SmallGlobalState {
+        game: Game::create(),
+        tick: 0,
+    })
 });
 
 fn config(config: GameConfig) -> GameConfig {
-   GameConfig {
-       resolution: ResolutionConfig::Physical(SCREEN_WIDTH, SCREEN_HEIGHT),
-       ..config
-   }
+    GameConfig {
+        resolution: ResolutionConfig::Physical(SCREEN_WIDTH, SCREEN_HEIGHT),
+        ..config
+    }
 }
 
 fn setup(_state: &mut GameState, _c: &mut EngineContext) {}
 
 fn update(_state: &mut GameState, _c: &mut EngineContext) {
-   let mut state = STATE.borrow_mut();   
+    let mut state = STATE.borrow_mut();
+    state.tick += 1;
 
-   if is_key_down(KeyCode::W) {
-       state.game._snake.set_speed(0.0, 1.0);
-   }
-   if is_key_down(KeyCode::S) {
-      state.game._snake.set_speed(0.0, -1.0);
-   }
-   if is_key_down(KeyCode::A) {
-      state.game._snake.set_speed(-1.0, 0.0);
-   }
-   if is_key_down(KeyCode::D) {
-      state.game._snake.set_speed(1.0, 0.0);
-   }
+    if is_key_down(KeyCode::W) {
+        state.game._snake.set_speed(0.0, 1.0);
+    }
+    if is_key_down(KeyCode::S) {
+        state.game._snake.set_speed(0.0, -1.0);
+    }
+    if is_key_down(KeyCode::A) {
+        state.game._snake.set_speed(-1.0, 0.0);
+    }
+    if is_key_down(KeyCode::D) {
+        state.game._snake.set_speed(1.0, 0.0);
+    }
 
-   state.game._snake.update();
-   state.game._snake.draw();
+    if state.tick % TICK == 0 {
 
-   //state.game._fruit.update();
-   state.game._fruit.draw();
+        state.game._snake.update();
 
+        let fx: f32 = state.game._fruit.px;
+        let fy: f32 = state.game._fruit.py;
+        if state.game._snake.check_eat(fx, fy) {
+            state.game._fruit.new_pos();
+        }
 
+        state.game._snake.draw();
+        state.game._fruit.draw();
+    }
+
+    draw_text(
+        format!(
+            "snake size:{}, tail:{}",
+            state.game._snake.size, state.game._snake.tail.len()
+        )
+        .as_str(),
+        vec2(-8.0, 14.0),
+        WHITE,
+        TextAlign::Center,
+    );
+    draw_text(
+        format!(
+            "fruit px:{}, py:{}",
+            state.game._fruit.px, state.game._fruit.py
+        )
+        .as_str(),
+        vec2(-8.0, 13.0),
+        WHITE,
+        TextAlign::Center,
+    );
 }
